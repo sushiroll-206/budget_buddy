82fde9273d681ece1f573605a13dc2ddacc8bbb4
leowjli <leo.wj.li@gmail.com>
2024-05-28 01:43:42 -0700

    fixed merge discreptancies, added favicon

--- public/javascripts/myBudget.js
+++ public/javascripts/myBudget.js
@@ -1,21 +1,22 @@
-async function init(){
+async function init() {
     await loadIdentity();
     loadUserBudget();
 }
 
-async function loadUserBudget(){
+async function loadUserBudget() {
     const urlParams = new URLSearchParams(window.location.search);
     const username = urlParams.get('user');
     console.log(username)
     if (!username) {
+        document.getElementById("username-span").innerText = `Please Log In`;
         console.error('No username found in url');
     }
 
-    if(username==myIdentity){
-        document.getElementById("username-span").innerText= `You (${username})`;
+    if (username == myIdentity) {
+        document.getElementById("username-span").innerText = `You (${username})`;
         document.getElementById("userBudgetDisplay").classList.remove("d-none");
-    }else{
-        document.getElementById("username-span").innerText=username;
+    } else {
+        document.getElementById("username-span").innerText = username;
         document.getElementById("userBudgetDisplay").classList.add("d-none");
     }
     
@@ -42,12 +43,12 @@ async function loadUserBudget(){
     loadUserInfoBudget(username)
 }
 
-async function loadUserInfoBudget(username){
+async function loadUserInfoBudget(username) {
     document.getElementById("userBudgetDisplay").innerText = "Loading...";
     let budgetJson = await fetchJSON(`api/${apiVersion}/myBudget?username=${encodeURIComponent(username)}`);
     console.log(budgetJson)
     actualBudgets = budgetJson.actualBudgets;
-    projectedBudgets= budgetJson.projectedBudgets;
+    projectedBudgets = budgetJson.projectedBudgets;
     // Divide budget into projected incomes and expenses
     let projectedIncomes = []
     let projectedExpenses = []
@@ -55,15 +56,15 @@ async function loadUserInfoBudget(username){
     // maps each budget instance as an income or expense -- under their respective category
     projectedBudgets.map(budgetInfo => {
         let transactionCategory = budgetInfo.category
-        if(budgetInfo.type == "Income"){
-            if(transactionCategory in projectedIncomes){
+        if (budgetInfo.type == "Income") {
+            if (transactionCategory in projectedIncomes) {
                 projectedIncomes[transactionCategory].push(budgetInfo)
             } else {
                 projectedIncomes[transactionCategory] = [budgetInfo]
             }
         }
-        if(budgetInfo.type == "Expense"){
-            if(transactionCategory in projectedExpenses){
+        if (budgetInfo.type == "Expense") {
+            if (transactionCategory in projectedExpenses) {
                 projectedExpenses[transactionCategory].push(budgetInfo)
                 // projectedExpenses.push({[transactionCategory]: budgetInfo})
             } else {
